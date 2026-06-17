7161a7fbf3683d90c85bab4de5f98ce51b2945b3
Suvendu Mondal <suvendumandal742@gmail.com>
2026-06-17 11:21:45 +0530

    Day 1: Added

---
+++ Week-01/Day-01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("Wellcome to Java Programming");
+        System.out.println("This is my first Java Program");
+    }
+}
---
+++ Week-01/Day-01/Introduction.java
@@ -0,0 +1,9 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Name : Suvendu Mondal");
+        System.out.println("Current Role : Php Delveloper");
+        System.out.println("Terget Role : Java Developer");
+        System.out.println("Terget Company : MNC");
+        
+    }
+}
